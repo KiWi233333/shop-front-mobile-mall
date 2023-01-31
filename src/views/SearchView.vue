e0a06965eed144557a075063af93cea36ca89400
kiwi233 <1329634286@qq.com>
2023-01-31 15:57:59 +0800

    完善搜索

---
+++ src/views/SearchView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="wind-init search-view">
+    <search-nav />
+    <goods-list :type="params" />
+  </div>
+</template>
+<script>
+import SearchNav from "@/components/Home/SearchNav.vue";
+import GoodsList from "@/components/Home/GoodsList.vue";
+export default {
+  components: { SearchNav, GoodsList },
+  name: "SearchView",
+  data() {
+    return {
+      params: { name: "" },
+    };
+  },
+  methods: {},
+};
+</script>
+<style scoped>
+</style>
